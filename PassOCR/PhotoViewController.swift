e6d7639dfbaf37fe0d54404ed078861b0d655c64
ParkerIndustries <mickael.k.swaminathan@gmail.com>
2019-11-08 21:43:47 +0100

    First commit

--- PassOCR/PhotoViewController.swift
+++ PassOCR/PhotoViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class PhotoViewController: UIViewController {
-
+    // MARK: - Storyboard
+    
+//    @IBOutlet weak var photoView: UIView!
+    
+    @IBOutlet weak var actionLabel: UILabel!
+    
+    // MARK: - Photo View Controller
+    /// Tous les utilisateurs scannés
+    var users: [User] = []
+    
+    /// Utilisateur actuellement en train de reconnaître
+    var currentUser: User? = nil
+    // MARK: Recognition
+    
+    // TODO: Create User w/ name & surname
+   
+    // TODO: Create timer
+    func createUser(name: String, surname: String) -> Void {
+        let new = User(name: name, surname: surname)
+        
+         // TODO: Show users values
+        
+        // TODO: Create timer
+        
+//        actionLabel.isHidden = true
+    }
+    
+    // MARK: - View Controller
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+//        actionLabel.text = "Bienvenue \(name)"
     }
     
 
---
+++ PassOCR/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  PassOCR
+//
+//  Created by Mickaël on 08/11/2019.
+//  Copyright © 2019 Marcus Florentin. All rights reserved.
+//
+
+import Foundation
+
+struct User: Hashable {
+    let name: String
+    let surname: String
+}
